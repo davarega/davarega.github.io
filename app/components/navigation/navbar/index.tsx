ee204cf22a6ff39a0f80b08e17591438edf233b5
davarega <xlithgt12@gmail.com>
2024-07-20 22:46:11 +0700

    menambah dokumentasi di navbar

--- app/components/navigation/navbar/index.tsx
+++ app/components/navigation/navbar/index.tsx
@@ -74,6 +74,7 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
 	return (
 		<div className="md:h-screen bg-gradient-to-r from-blue-base to-purple-base md:from-purple-mate md:via-blue-mate md:to-blue-dash flex flex-col justify-between">
 			{/* <div className={`${scroll ? 'navbar-top active' : 'navbar-top'} w-full top-0 z-[9]`}> */}
+			{/* Navbar Atas */}
 			<div className="w-full z-9">
 				<div className="container mx-auto px-4 h-20">
 					<div className="flex justify-between items-center h-full">
@@ -84,6 +85,7 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
 								</h1>
 							</Link>
 						</div>
+						{/* Tombol navbar android */}
 						<button
 							type="button"
 							className="inline-flex items-center md:hidden"
@@ -91,6 +93,7 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
 						>
 							<FaBars />
 						</button>
+						{/* Navbar atas kanan */}
 						<ul className="hidden md:flex gap-x-6 text-white items-center">
 							<li>
 								<Link href="/">
@@ -111,31 +114,26 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
 					</div>
 				</div>
 			</div>
+			{/* Tulisan di tengah */}
 			<div>
 				<h1 className="hidden md:flex justify-center font-quintessential h-full items-center flex text-5xl">
 					WELCOME TO ASEP NEWS !
 				</h1>
 			</div>
+			{/* Navbar bawah */}
 			<div className="hidden md:flex w-full bottom-0">
 				<div className="container h-20 mx-auto px-4">
 					<ul className="hidden md:flex gap-x-6 text-white flex justify-between items-center h-full">
+						{/* Load Navbar dari data */}
 						{navItems.map((item, index) => (
 							<li className="group h-full">
-								<Link key={index} href="/" className="h-full flex items-center">
+								<Link key={index} href={item.link ?? "#"} className="h-full flex items-center">
 									<p className="flex gap-2">
 										<span>{item.label}</span>
 										{item.children && <FaAngleDown className="text-xl group-hover:rotate-180 transition-all" />}
 									</p>
-									{/* {item.children && (
-										<div className="absolute right-0 text-black hidden group-hover:flex w-auto flex-col gap-1 transition-all bg-white py-3">
-											{item.children.map((child, index) => (
-												<Link key={index} href={child.link ?? "#"}>
-													<p>{child.label}</p>
-												</Link>
-											))}
-										</div>
-									)} */}
 								</Link>
+								{/* Load dropdown jika ada */}
 								{item.children && (
 									<div className="absolute -mt-5 text-black transition duration-300 rounded-xl hidden group-hover:flex w-auto flex-col gap-4 transition-all bg-white py-3 px-4">
 										{item.children.map((child, index) => (
@@ -147,48 +145,9 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
 								)}
 							</li>
 						))}
-						{/* <li>
-							<Link href="/">
-								<p>Home</p>
-							</Link>
-						</li>
-						<li>
-							<Link href="/">
-								<p>Nasional</p>
-							</Link>
-						</li>
-						<li>
-							<Link href="/">
-								<p>Internasional</p>
-							</Link>
-						</li>
-						<li>
-							<Link href="/">
-								<p>Ekonomi</p>
-							</Link>
-						</li>
-						<li>
-							<Link href="/">
-								<p>Olahraga</p>
-							</Link>
-						</li>
-						<li>
-							<Link href="/" className="relative items-center group">
-								<p className="flex gap-2">
-									<span>Lainnya</span>
-									<FaAngleDown className="text-xl group-hover:rotate-180 transition-all" />
-								</p>
-								<div className="absolute right-0 text-black hidden group-hover:flex w-auto flex-col gap-1 transition-all bg-white py-3">
-									hello
-								</div>
-							</Link>
-						</li> */}
 					</ul>
 				</div>
 			</div>
-			{/* <div className="mx-auto bg-gradient-to-r from-purple-mate via-blue-mate to-blue-dash">
-				
-			</div> */}
 		</div>
 	);
 };
